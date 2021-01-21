5d101628c5fed133461e86c4f6dc65912e9f051b
albhasan <albhasan@gmail.com>
2021-01-21 23:22:46 +0000

    Updated extent of crop.

--- scripts/crop_gfw.sh
+++ scripts/crop_gfw.sh
@@ -5,7 +5,8 @@ in_file="/home/alber.ipia/Documents/data/global_forest_watch/Hansen_GFC-2019-v1.
 out_file="/home/alber.ipia/Documents/data/global_forest_watch/Hansen_GFC-2019-v1.7_lossyear_10S_070W_077095.tif"
 
 # BBOX BDC tile small 077095
-coords="-65.2752799481130950 -10.9812035370481702 -63.6803303411144910 -9.9471581104979609"
+coords="-65.2752794737273661 -10.9811925085195607 -63.6803226757691903 -9.9471581104979627"
+
 
 # Set the PRODES' EPSG
 srs="-s_srs EPSG:4326 -t_srs EPSG:4326"
--- scripts/crop_prodes.sh
+++ scripts/crop_prodes.sh
@@ -5,7 +5,7 @@ in_file="/home/alber.ipia/Documents/data/prodes/2019/PDigital2000_2019_AMZ_gtif.
 out_file="/home/alber.ipia/Documents/data/prodes/2019/PDigital2000_2019_AMZ_gtif_077095.tif"
 
 # BBOX BDC tile small 077095
-coords="-65.2752799481130950 -10.9812035370481702 -63.6803303411144910 -9.9471581104979609"
+coords="-65.2752794737273661 -10.9811925085195607 -63.6803226757691903 -9.9471581104979627"
 
 # Set the PRODES' EPSG
 srs="-s_srs EPSG:4674 -t_srs EPSG:4674"
